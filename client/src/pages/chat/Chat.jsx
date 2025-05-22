2f925c01562f4bf44cc9f499f04e3834fcd88640
OS <os@OSs-Mac-mini.local>
2025-05-22 14:53:47 -0500

    finishing up frontend

--- client/src/pages/chat/Chat.jsx
+++ client/src/pages/chat/Chat.jsx
@@ -1,9 +1,78 @@
 import React from 'react'
 import "./Chat.css"
+import NewPrompt from '../../components/newPrompt/NewPrompt';
 
 const Chat = () => {
+  
   return (
-    <div>Chat</div>
+    <div className="chatMain">
+      <div className="chatWrapper">
+        <div className="chat">
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          <div className="message">
+            I am sentient now
+          </div>
+          <div className="message user">
+            This is ok
+          </div>
+          
+          <NewPrompt />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='home'>
       <h1 className='logoText'>CHAT CPT</h1>
-      <h2 className='homeText'>Get answers. Find inspiration, Be more productie.</h2>
+      <h2 className='homeText'>Get answers. Find inspiration, Be more productive.</h2>
       <h3 className='subHomeText'>Free to use. Easy to try. Just ask and Chat CPT can help with writing, learning, brainstorming, and more.</h3>
       
       <Link className = 'dashboardButton' to={"/dashboard"}>Start Now</Link>
